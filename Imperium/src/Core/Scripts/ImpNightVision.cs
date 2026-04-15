96469bc9bd8a526045e8890e8c78e3f1c2496a36
ivan tkachenko <ratijas.tkachenko@gmail.com>
2026-04-16 01:03:11 +0300

    Night Vision: refactor to store objects and components in a struct
    For the sake of bookkeeping let's store all relevant objects in an
    ad-hoc record structure instead of half a dozen flat members. This will
    become more impactful with a new upcoming component.
    
    Also sort the two lights consistently: Near, then Far.

--- Imperium/src/Core/Scripts/ImpNightVision.cs
+++ Imperium/src/Core/Scripts/ImpNightVision.cs
@@ -1,6 +1,7 @@
 #region
 
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
 #endregion
 
@@ -8,34 +9,50 @@ namespace Imperium.Core.Scripts;
 
 public class ImpNightVision : MonoBehaviour
 {
-    private Light FarLight;
-    private Light NearLight;
+    private ImpNightVisionLight Near;
+    private ImpNightVisionLight Far;
 
     internal static ImpNightVision Create() => new GameObject("Imp_NightVision").AddComponent<ImpNightVision>();
 
     private void Awake()
     {
         var mapCamera = GameObject.Find("MapCamera").transform;
+        var position = mapCamera.position;
         transform.SetParent(Imperium.Player.gameplayCamera.transform);
 
-        NearLight = new GameObject("Near").AddComponent<Light>();
-        NearLight.transform.SetParent(transform);
-        NearLight.transform.position = mapCamera.position + Vector3.down * 80f;
-        NearLight.range = 70f;
-        NearLight.color = new Color(0.875f, 0.788f, 0.791f, 1);
+        Near = SetupLight("Near", transform);
+        Near.GameObject.transform.position = position + Vector3.down * 80f;
+        Near.Light.range = 70f;
+        Near.Light.color = new Color(0.875f, 0.788f, 0.791f, 1);
 
-        FarLight = new GameObject("Far").AddComponent<Light>();
-        FarLight.transform.SetParent(transform);
-        FarLight.transform.position = mapCamera.position + Vector3.down * 30f;
-        FarLight.range = 500f;
+        Far = SetupLight("Far", transform);
+        Far.GameObject.transform.position = position + Vector3.down * 30f;
+        Far.Light.range = 500f;
+    }
+
+    private static ImpNightVisionLight SetupLight(string gameObjectName, Transform parent)
+    {
+        var obj = new GameObject(gameObjectName);
+        obj.transform.SetParent(parent);
+        return new()
+        {
+            GameObject = obj,
+            Light = obj.AddComponent<Light>(),
+        };
     }
 
     private void Update()
     {
-        FarLight.enabled = Imperium.Player.nightVision.enabled;
-        NearLight.enabled = Imperium.Player.nightVision.enabled;
+        Near.Light.enabled = Imperium.Player.nightVision.enabled;
+        Far.Light.enabled = Imperium.Player.nightVision.enabled;
 
-        NearLight.intensity = Imperium.Settings.Player.NightVision.Value * 100f;
-        FarLight.intensity = Imperium.Settings.Player.NightVision.Value * 1100f;
+        Near.Light.intensity = Imperium.Settings.Player.NightVision.Value * 100f;
+        Far.Light.intensity = Imperium.Settings.Player.NightVision.Value * 1100f;
     }
+}
+
+internal record ImpNightVisionLight
+{
+    internal GameObject GameObject { get; init; }
+    internal Light Light { get; init; }
 }
